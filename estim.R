979ac2fe31fd83e088fd91ef50d8640926001dc6
Hektor Dannyel <hektor.brasil@gmail.com>
2018-07-03 21:20:02 -0300

    termina

--- estim.R
+++ estim.R
@@ -33,23 +33,23 @@ esp2 <- function(x_p){
 
 integrate(esp, 0, Inf)$value - integrate(esp, 0, Inf)$value^2
 
-# Estimando moda e mediana da preditiva
+# Estimando moda e quantis da preditiva
 
 posteriori <- rgamma(1000, shape = n, scale = 1/sx)
 yp <- rexp(1000, posteriori)
 
-quant <- quantile(yp)
+quant <- quantile(yp) # Quantis
 
 dens <- density(yp)
 
-moda <- dens$x[which(dens$y == max(dens$y))]
+moda <- dens$x[which(dens$y == max(dens$y))] # Moda
+
+# Solução analítica vs simulação
 
 x_axis <- seq(-1, 8, len = 1000)
 pred1 <- pred(x_axis, sx, n)
 plot(x_axis, pred1, col = "red", lty = 2, type = "l")
 lines(density(yp))
-
-density(yp)
 
 
 ###
@@ -65,6 +65,14 @@ theta <- rlnorm(1000, mu, vari)
 
 pred_pois <- rpois(1000, theta)
 
-curve(dnorm(x, mean = mean(pred_pois), sd = sd(pred_pois)), from = 0, to = 12)
+x <- seq(from = 0, to = 12, l = 1000)
 
-lines(prop.table(table(pred_pois)), type = "h")
+plot(dnorm(x, mean = mean(pred_pois), sd = sd(pred_pois)) ~ x, t = "l", ylim = c(0, .4),
+     ylab = "")
+
+lines(prop.table(table(pred_pois)), type = "h", col = "red", lty = 3)
+
+lines(dnorm(c(0:12), mean = mean(pred_pois), sd = sd(pred_pois)) ~ c(0:12), type = "h", col = "green")
+
+legend("topright", col = c("black", "red", "green"), lty = c(1, 3, 1), 
+       legend = c("Aproximação normal", "Simulação", "Discretização"))
