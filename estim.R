a251365b154a02c856ffd7bfa4ec4f59647ec763
Hektor Dannyel <hektor.brasil@gmail.com>
2018-07-06 00:26:46 -0300

    Finaliza

--- estim.R
+++ estim.R
@@ -1,3 +1,5 @@
+### Exponencial
+
 # Função preditiva
 
 pred <- function(x_p, sx, n){
@@ -26,7 +28,7 @@ integrate(esp, 0, Inf)
 # Esperança x^2
 
 esp2 <- function(x_p){
-  n*(sum(x)/(x_p^2 + sum(x)))^n*1/(x_p^2 + sum(x)) * x_p^2
+  n*(sum(x)/(x_p + sum(x)))^n*1/(x_p + sum(x)) * x_p^2
 }
 
 # Variância
@@ -38,11 +40,14 @@ integrate(esp, 0, Inf)$value - integrate(esp, 0, Inf)$value^2
 posteriori <- rgamma(1000, shape = n, scale = 1/sx)
 yp <- rexp(1000, posteriori)
 
-quant <- quantile(yp) # Quantis
+quant <- quantile(yp); quant # Quantis
 
 dens <- density(yp)
 
-moda <- dens$x[which(dens$y == max(dens$y))] # Moda
+moda <- dens$x[which(dens$y == max(dens$y))]; moda # Moda
+
+plot(dens)
+abline(v = moda, h = max(dens$y))
 
 # Solução analítica vs simulação
 
@@ -50,27 +55,33 @@ x_axis <- seq(-1, 8, len = 1000)
 pred1 <- pred(x_axis, sx, n)
 plot(x_axis, pred1, col = "red", lty = 2, type = "l")
 lines(density(yp))
+legend("topright", legend = c("Analítico", "Simulação"), lty = c(2, 1), col = c("red", "black"))
 
+### log-normal
 
-###
+# Parâmetros da priori
 
-mu <- 0
+mu <- 0 
 vari <- 1
 
-post <- function(x, mu, vari, n, sx){
-  exp(-n*x-(log(x)-mu)^2/(2*vari))*x^(sx-1)
-}
+theta <- rlnorm(1000, mu, vari) # Simulando valores da priori
 
-theta <- rlnorm(1000, mu, vari)
+# Simulando valores da preditiva
 
-pred_pois <- rpois(1000, theta)
+pred_pois <- rpois(1000, theta) 
 
 x <- seq(from = 0, to = 12, l = 1000)
+
+# Curva da aproximação normal
 
 plot(dnorm(x, mean = mean(pred_pois), sd = sd(pred_pois)) ~ x, t = "l", ylim = c(0, .4),
      ylab = "")
 
+# Valores simulados
+
 lines(prop.table(table(pred_pois)), type = "h", col = "red", lty = 3)
+
+# Discretização
 
 lines(dnorm(c(0:12), mean = mean(pred_pois), sd = sd(pred_pois)) ~ c(0:12), type = "h", col = "green")
 
